91eecf7383f699e1145180fa465c82b15deb87e4
KaylaBtv22 <kaylabutv@gmail.com>
2025-09-28 14:03:23 -0400

    message

--- ros2-ws/src/network-comms/src/net-node.cpp
+++ ros2-ws/src/network-comms/src/net-node.cpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <thread>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -11,34 +12,32 @@ using namespace std::chrono_literals;
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
+class NetworkNode : public rclcpp::Node
 {
   public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
-    {
+    NetworkNode()
+    : Node("Network_Node"), count_(0){
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      500ms, std::bind(&NetworkNode::timer_callback, this));
     }
 
   private:
-    void timer_callback()
-    {
+    void timer_callback(){
       auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
+      message.data = buffer;
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+
+    char[1024] buffer;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]){
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<NetworkNode>());
   rclcpp::shutdown();
   return 0;
 }
