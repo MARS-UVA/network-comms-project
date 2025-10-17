07ac49c884193c4735a6274b0be10ab570e7ecd4
Eric Paschke <edpaschke@gmail.com>
2025-10-16 20:09:50 -0400

    added dataHeader struct

--- ros2-ws/src/network-comms/src/main.hpp
+++ ros2-ws/src/network-comms/src/main.hpp
@@ -6,3 +6,14 @@ struct ThreadInfo{
 int create_server (ThreadInfo* info);
 
 void client_send(unsigned char *data, size_t bytes_to_send);
+
+struct DataHeader
+{
+    uint16_t dataType;
+    uint16_t packetNum;
+    uint16_t totalPackets;
+    uint16_t fragment_size;
+    uint16_t crc;
+} __attribute__((packed));
+#define HEADER_SIZE sizeof(DataHeader)
+typedef struct DataHeader DataHeader;
